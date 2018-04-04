04153fb96c26dd25d75ffcca7a8c64ba06662fcf
Sumeet Bansal <sumeetbansal@gmail.com>
2018-04-03 20:08:49 -0700

    added -y flag to env-setup.sh installs

--- env-setup.sh
+++ env-setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-get install vim git make
-sudo apt install ruby
+sudo apt-get install vim git make -y
+sudo apt install ruby -y
 sudo gem install bundler
-sudo apt-get install ruby-dev zlib1g-dev
+sudo apt-get install ruby-dev zlib1g-dev -y
 sudo bundle update
 sudo bundle install
 bundle exec jekyll serve
